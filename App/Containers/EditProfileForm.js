7b4a31526c578ff90dc569349d69d9b9295ab7f1
Ismael de Esteban <ismael.deesteban@gmail.com>
2018-02-03 10:45:44 +0100

    Revert breaking changes

--- App/Containers/EditProfileForm.js
+++ App/Containers/EditProfileForm.js
@@ -22,19 +22,12 @@ import {
   Item,
   Label
 } from 'native-base'
-import {
-  connect
-} from 'react-redux'
+import { connect } from 'react-redux'
 import Styles from './Styles/EditProfileFormStyle'
-import {
-  Images,
-  Metrics
-} from '../Themes'
+import {Images, Metrics} from '../Themes'
 import MapView from 'react-native-maps'
 import UsersActions from '../Redux/UsersRedux'
-import {
-  Actions as NavigationActions
-} from 'react-native-router-flux'
+import { Actions as NavigationActions } from 'react-native-router-flux'
 import I18n from 'react-native-i18n'
 import ProfilePhotoUploader from './ProfilePhotoUploader'
 import CommonHeader from '../Components/CommonHeader'
@@ -71,7 +64,7 @@ class EditProfileForm extends React.Component {
   keyboardDidShowListener: Object
   keyboardDidHideListener: Object
 
-  constructor(props: ProfilePostProps) {
+  constructor (props: ProfilePostProps) {
     super(props)
     this.state = {
       name: '',
@@ -87,18 +80,18 @@ class EditProfileForm extends React.Component {
     this.isAttempting = false
   }
 
-  assignProfileToState(profile) {
+  assignProfileToState (profile) {
     this.state.name = profile.name
     this.state.email = profile.email
     this.state.phone = profile.phone
     this.state.telegramId = profile.telegram_id
     this.state.faircoinAddress = profile.faircoin_address
-    this.state.uuid = profile.uuid
+    this.state.uuid= profile.uuid
     this.state.longitude = parseFloat(profile.location.longitude)
     this.state.latitude = parseFloat(profile.location.latitude)
   }
 
-  componentWillMount() {
+  componentWillMount () {
     // Using keyboardWillShow/Hide looks 1,000 times better, but doesn't work on Android
     // TODO: Revisit this if Android begins to support - https://github.com/facebook/react-native/issues/3468 or https://github.com/facebook/react-native/issues/14275
     this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.keyboardDidShow)
@@ -107,7 +100,7 @@ class EditProfileForm extends React.Component {
     this.assignProfileToState(this.props.profile)
   }
 
-  componentWillUnmount() {
+  componentWillUnmount () {
     this.keyboardDidShowListener.remove()
     this.keyboardDidHideListener.remove()
   }
@@ -118,10 +111,7 @@ class EditProfileForm extends React.Component {
     let newSize = Metrics.screenHeight - e.endCoordinates.height
     this.setState({
       visibleHeight: newSize,
-      topLogo: {
-        width: 100,
-        height: 70
-      }
+      topLogo: {width: 100, height: 70}
     })
   }
 
@@ -130,133 +120,83 @@ class EditProfileForm extends React.Component {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
     this.setState({
       visibleHeight: Metrics.screenHeight,
-      topLogo: {
-        width: Metrics.screenWidth
-      }
+      topLogo: {width: Metrics.screenWidth}
     })
   }
 
   handlePressPost = () => {
-
-    var {
-      name,
-      email,
-      phone,
-      telegramId,
-      faircoinAddress,
-      longitude,
-      latitude,
-      uuid
-    } = this.state
-
+    var { name, email, phone, telegramId, faircoinAddress, uuid } = this.state
     this.isAttempting = true
+    // attempt a login - a saga is listening to pick it up from here.
 
-    this.props.attemptProfilePost(name, email, phone, telegramId,
-      faircoinAddress, longitude, latitude, uuid)
+    this.props.attemptProfilePost(name, email, phone, telegramId, faircoinAddress, uuid)
   }
 
   handleChangeName = (text) => {
-    this.setState({
-      name: text
-    })
+    this.setState({ name: text })
   }
 
   handleChangeEmail = (text) => {
-    this.setState({
-      email: text
-    })
+    this.setState({ email: text })
   }
 
   handleChangePhone = (text) => {
-    this.setState({
-      phone: text
-    })
+    this.setState({ phone: text })
   }
 
-  handleChangeTelegramId = (text) => {
-    this.setState({
-      telegramId: text
-    })
+  handleChangeTelegramId= (text) => {
+    this.setState({ telegramId: text })
   }
 
   handleChangeFaircoinAddress = (text) => {
-    this.setState({
-      faircoinAddress: text
-    })
+    this.setState({ faircoinAddress: text })
   }
 
   handleChangeMap = (region) => {
-    this.setState({
-      longitude: region.longitude
-    })
-    this.setState({
-      latitude: region.latitude
-    })
+    this.setState( { longitude: region.longitude})
+    this.setState({latitude: region.latitude})
   }
 
-  renderPublishButtonText() {
+  renderPublishButtonText () {
     if (this.props.posting) {
-      return ( <
-        Text style = {
-          Styles.buttonText
-        } > {
-          I18n.t('Publishing')
-        } < /Text>
+      return (
+        <Text style={Styles.buttonText}>{I18n.t('Publishing')}</Text>
       )
     } else {
-      return ( <
-        Text style = {
-          Styles.buttonText
-        } > {
-          I18n.t('Published')
-        } < /Text>
+      return (
+        <Text style={Styles.buttonText}>{I18n.t('Published')}</Text>
       )
     }
   }
 
-  renderFaircoinAddressInput(faircoinAddress, editable) {
+  renderFaircoinAddressInput (faircoinAddress, editable) {
     if (AppConfig.FaircoinEnabled) {
-      return ( <
-        Item floatingLabel >
-        <
-        Label > {
-          I18n.t('Faircoin Address')
-        } < /Label> <
-        Input ref = 'faircoinAddress'
-        value = {
-          faircoinAddress
-        }
-        editable = {
-          editable
-        }
-        keyboardType = 'default'
-        returnKeyType = 'next'
-        autoCapitalize = 'sentences'
-        onChangeText = {
-          this.handleChangeFaircoinAddress
-        }
-        numberOfLines = {
-          8
-        }
-        underlineColorAndroid = 'transparent'
-        onSubmitEditing = {
-          () => this.refs.phone.focus()
-        }
-        /> <
-        Text style = {
-          Styles.errorLabel
-        } > {
-          (this.props.error && this.props.error.faircoin_address) ? this.props.error['faircoin_address'][0] : ''
-        } <
-        /Text> <
-        /Item>
-      )
+    return (
+      <Item floatingLabel>
+        <Label>{I18n.t('Faircoin Address')}</Label>
+        <Input
+          ref='faircoinAddress'
+          value={faircoinAddress}
+          editable={editable}
+          keyboardType='default'
+          returnKeyType='next'
+          autoCapitalize='sentences'
+          onChangeText={this.handleChangeFaircoinAddress}
+          numberOfLines={8}
+          underlineColorAndroid='transparent'
+          onSubmitEditing={() => this.refs.phone.focus()}
+          />
+        <Text style={Styles.errorLabel}>
+          { (this.props.error && this.props.error.faircoin_address) ? this.props.error['faircoin_address'][0] : ''}
+        </Text>
+      </Item>
+    )
     } else {
-      return ( < Item / > )
+      return (<Item />)
     }
   }
 
-  render() {
+  render () {
     const {
       name,
       email,
@@ -267,231 +207,126 @@ class EditProfileForm extends React.Component {
       latitude
     } = this.state
     const editable = !this.props.posting
-    return ( <
-      Container >
-      <
-      CommonHeader title = {
-        I18n.t('Edit Profile')
-      }
-      /> <
-      Content padder >
-      <
-      Card >
-      <
-      NamedLogo / >
-      <
-      ProfilePhotoUploader user = {
-        this.props.profile
-      }
-      /> <
-      Form >
-      <
-      Item floatingLabel >
-      <
-      Label > {
-        I18n.t('Name')
-      } < /Label> <
-      Input ref = 'name'
-      value = {
-        name
-      }
-      editable = {
-        editable
-      }
-      keyboardType = 'default'
-      returnKeyType = 'next'
-      autoCapitalize = 'words'
-      autoCorrect = {
-        false
-      }
-      onChangeText = {
-        this.handleChangeName
-      }
-      underlineColorAndroid = 'transparent'
-      onSubmitEditing = {
-        () => this.refs.email.focus()
-      }
-      /> <
-      /Item> <
-      Text style = {
-        Styles.errorLabel
-      } > {
-        (this.props.error && this.props.error.name) ? this.props.error['name'][0] : ''
-      } <
-      /Text>
+    return (
+      <Container>
+        <CommonHeader title={I18n.t('Edit Profile')} />
+        <Content padder>
+          <Card>
+            <NamedLogo />
+            <ProfilePhotoUploader user={this.props.profile} />
+            <Form>
+              <Item floatingLabel>
+                <Label>{I18n.t('Name')}</Label>
+                <Input
+                  ref='name'
+                  value={name}
+                  editable={editable}
+                  keyboardType='default'
+                  returnKeyType='next'
+                  autoCapitalize='words'
+                  autoCorrect={false}
+                  onChangeText={this.handleChangeName}
+                  underlineColorAndroid='transparent'
+                  onSubmitEditing={() => this.refs.email.focus()}
+                  />
+              </Item>
+                <Text style={Styles.errorLabel}>
+                  { (this.props.error && this.props.error.name) ? this.props.error['name'][0] : ''}
+                </Text>
 
-      <
-      Item floatingLabel >
-      <
-      Label > {
-        I18n.t('Email')
-      } < /Label> <
-      Input ref = 'email'
-      value = {
-        email
-      }
-      editable = {
-        editable
-      }
-      keyboardType = 'email-address'
-      returnKeyType = 'next'
-      autoCapitalize = 'sentences'
-      autoCorrect onChangeText = {
-        this.handleChangeEmail
-      }
-      numberOfLines = {
-        8
-      }
-      underlineColorAndroid = 'transparent'
-      onSubmitEditing = {
-        () => this.refs.telegramId.focus()
-      }
-      /> <
-      /Item> <
-      Text style = {
-        Styles.errorLabel
-      } > {
-        (this.props.error && this.props.error.email) ? this.props.error['email'][0] : ''
-      } <
-      /Text>
+            <Item floatingLabel>
+              <Label>{I18n.t('Email')}</Label>
+              <Input
+                ref='email'
+                value={email}
+                editable={editable}
+                keyboardType='email-address'
+                returnKeyType='next'
+                autoCapitalize='sentences'
+                autoCorrect
+                onChangeText={this.handleChangeEmail}
+                numberOfLines={8}
+                underlineColorAndroid='transparent'
+                onSubmitEditing={() => this.refs.telegramId.focus()}
+                />
+            </Item>
+              <Text style={Styles.errorLabel}>
+                { (this.props.error && this.props.error.email) ? this.props.error['email'][0] : ''}
+              </Text>
 
-      <
-      Item floatingLabel >
-      <
-      Label > {
-        I18n.t('Telegram Id')
-      } < /Label> <
-      Input ref = 'telegramId'
-      value = {
-        telegramId
-      }
-      editable = {
-        editable
-      }
-      keyboardType = 'email-address'
-      returnKeyType = 'next'
-      autoCapitalize = 'sentences'
-      onChangeText = {
-        this.handleChangeTelegramId
-      }
-      numberOfLines = {
-        8
-      }
-      underlineColorAndroid = 'transparent'
-      onSubmitEditing = {
-        () => this.refs.phone.focus()
-      }
-      /> <
-      /Item> <
-      Text style = {
-        Styles.errorLabel
-      } > {
-        (this.props.error && this.props.error.telegram_id) ? this.props.error['telegram_id'][0] : ''
-      } <
-      /Text>
+          <Item floatingLabel>
+            <Label>{I18n.t('Telegram Id')}</Label>
+            <Input
+              ref='telegramId'
+              value={telegramId}
+              editable={editable}
+              keyboardType='email-address'
+              returnKeyType='next'
+              autoCapitalize='sentences'
+              onChangeText={this.handleChangeTelegramId}
+              numberOfLines={8}
+              underlineColorAndroid='transparent'
+              onSubmitEditing={() => this.refs.phone.focus()}
+              />
+          </Item>
+            <Text style={Styles.errorLabel}>
+              { (this.props.error && this.props.error.telegram_id) ? this.props.error['telegram_id'][0] : ''}
+            </Text>
 
-      <
-      Item floatingLabel >
-      <
-      Label > {
-        I18n.t('Phone')
-      } < /Label> <
-      Input ref = 'phone'
-      value = {
-        phone
-      }
-      editable = {
-        editable
-      }
-      keyboardType = 'phone-pad'
-      returnKeyType = 'next'
-      autoCorrect = {
-        false
-      }
-      onChangeText = {
-        this.handleChangePhone
-      }
-      underlineColorAndroid = 'transparent'
-      onSubmitEditing = {
-        this.handlePressPost
-      }
-      /> <
-      /Item> <
-      Text style = {
-        Styles.errorLabel
-      } > {
-        (this.props.error && this.props.error.phone) ? this.props.error['phone'][0] : ''
-      } <
-      /Text> <
-      Text style = {
-        Styles.errorLabel
-      } > {
-        (this.props.error && this.props.error.non_field_errors) ? this.props.error['non_field_errors'][0] : ''
-      } <
-      /Text>
+          <Item floatingLabel>
+            <Label>{I18n.t('Phone')}</Label>
+            <Input
+              ref='phone'
+              value={phone}
+              editable={editable}
+              keyboardType='phone-pad'
+              returnKeyType='next'
+              autoCorrect={false}
+              onChangeText={this.handleChangePhone}
+              underlineColorAndroid='transparent'
+              onSubmitEditing={this.handlePressPost}
+              />
+          </Item>
+          <Text style={Styles.errorLabel}>
+            { (this.props.error && this.props.error.phone) ? this.props.error['phone'][0] : ''}
+          </Text>
+          <Text style={Styles.errorLabel}>
+            { (this.props.error && this.props.error.non_field_errors) ? this.props.error['non_field_errors'][0] : ''}
+          </Text>
 
-      {
-        this.renderFaircoinAddressInput(faircoinAddress, editable)
-      }
+          {this.renderFaircoinAddressInput(faircoinAddress, editable)}
 
-      <
-      Content style = {
-        Styles.mapSection
-      } >
-      <
-      Label > {
-        I18n.t('Where do you offer your services:')
-      } < /Label> <
-      MapView style = {
-        Styles.map
-      }
-      initialRegion = {
-        {
-          latitude: latitude,
-          longitude: longitude,
-          latitudeDelta: 0.032,
-          longitudeDelta: 0.031
-        }
-      }
-      onRegionChangeComplete = {
-        this.handleChangeMap
-      } >
-      <
-      MapView.Marker coordinate = {
-        {
-          latitude: latitude,
-          longitude: longitude
-        }
-      }
-      title = {
-        "title"
-      }
-      description = {
-        "description"
-      }
-      /> <
-      /MapView> <
-      /Content>
+          <Content style={Styles.mapSection}>
+            <Label>{I18n.t('Where do you offer your services:')}</Label>
+            <MapView
+              style={Styles.map}
+              initialRegion={{
+                latitude: latitude,
+                longitude: longitude,
+                latitudeDelta: 0.032,
+                longitudeDelta: 0.031
+              }}
+              onRegionChangeComplete={this.handleChangeMap}
+              >
+              <MapView.Marker
+                coordinate={{
+                  latitude: latitude,
+                  longitude: longitude}}
+                title={"title"}
+                description={"description"}
+              />
+            </MapView>
+          </Content>
 
-      <
-      /Form> <
-      TouchableOpacity style = {
-        Styles.loginButtonWrapper
-      }
-      onPress = {
-        this.handlePressPost
-      } >
-      <
-      CardItem style = {
-        Styles.buttonCta
-      } > {
-        this.renderPublishButtonText()
-      } <
-      /CardItem> <
-      /TouchableOpacity> <
-      /Card> <
-      /Content> <
-      /Container>
+          </Form>
+          <TouchableOpacity style={Styles.loginButtonWrapper} onPress={this.handlePressPost}>
+            <CardItem style={Styles.buttonCta}>
+              {this.renderPublishButtonText()}
+            </CardItem>
+          </TouchableOpacity>
+          </Card>
+        </Content>
+      </Container>
     )
   }
 }
@@ -514,8 +349,6 @@ const mapDispatchToProps = (dispatch) => {
           phone,
           telegramId,
           faircoinAddress,
-          longitude,
-          latitude,
           uuid
         )
       )
